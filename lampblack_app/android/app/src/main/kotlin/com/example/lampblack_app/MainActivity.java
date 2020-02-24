d4eea582b1f3df72463d31653b3a4a8053e91792
shuangjian.zhou <zhoushuangjian@blhcn.com>
2020-02-24 13:06:26 +0800

    ss

--- lampblack_app/android/app/src/main/kotlin/com/example/lampblack_app/MainActivity.java
+++ lampblack_app/android/app/src/main/kotlin/com/example/lampblack_app/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends FlutterActivity {
 
     @Override
     public void configureFlutterEngine(@NonNull FlutterEngine flutterEngine) {
-        GeneratedPluginRegistrant.registerWith(flutterEngine);
+        GeneratedPluginRegistrant.registerWith((PluginRegistry) flutterEngine);
         new MethodChannel(flutterEngine.getDartExecutor().getBinaryMessenger(), CHANNEL)
         .setMethodCallHandler(
           (call, result) -> {
